5ce987cfeeed5dcde2d682ec128e1c209f737583
tarce <terek.arce@gmail.com>
2014-04-12 22:16:52 -0400

    updated binomial heap

--- src/leftVsBi/binomialHeap/BinomialHeap.java
+++ src/leftVsBi/binomialHeap/BinomialHeap.java
@@ -1,5 +1,76 @@
 package leftVsBi.binomialHeap;
 
-public class BinomialHeap {
+import leftVsBi.dataStruct.EmptyException;
 
+public class BinomialHeap {
+	
+	Node min;
+	int maxDegree;
+	
+	public BinomialHeap() {
+		
+	}
+	
+	public BinomialHeap( int maxDegree ) {
+		
+	}
+	
+	public void push ( int element ) {
+		
+		// add new node to top level list
+		if (min != null) {
+			Node temp = min.sibling;
+			min.sibling = new Node( element );
+			min.sibling.sibling = temp;
+			if (min.element > min.sibling.element) {
+				min = min.sibling;
+			}
+		}
+		
+		// otherwise, create top level list of just the new min
+		else {
+			min = new Node( element );
+			min.sibling = min;
+		}
+		
+	}
+	
+	public void removeMin() {
+		
+		// if heap exists
+		if (min != null) {
+			
+			Node temp = min.child;
+			
+			// if there are children
+			if (temp != null) {
+				
+			}
+			
+			// if there are no children (only top level list)
+			else {
+				
+				// combine the new top level trees
+				if (min != min.sibling) {
+					min->copy(min->sibling);
+					pairWiseCombine();
+				}
+				// no tree is left so set min back to null
+				else {
+					min = NULL;
+				}
+				
+				
+			}
+			
+			
+			
+			
+		}
+		else {
+			throw new EmptyException();
+		}
+		
+	}
+	
 }
